4fdd40a4ddffd1da317835519fecd8c77ea7fb3f
Crtruban <55709830+Crtruban@users.noreply.github.com>
2020-11-14 15:51:09 -0500

    Update MyServerSocket.java

--- Assignment1/src/com/pgx/java/socket/MyServerSocket.java
+++ Assignment1/src/com/pgx/java/socket/MyServerSocket.java
@@ -1,6 +1,7 @@
 package com.pgx.java.socket;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,6 +15,59 @@ public class MyServerSocket {
         else 
           this.server = new ServerSocket(0, 1, InetAddress.getLocalHost());
     }
+
+	   public static byte hexToByte(String hexString) {
+	        int firstDigit = toDigit(hexString.charAt(0));
+	        int secondDigit = toDigit(hexString.charAt(1));
+	        return (byte) ((firstDigit << 4) + secondDigit);
+	    }
+	    
+	    private static int toDigit(char hexChar) {
+	        int digit = Character.digit(hexChar, 16);
+	        if(digit == -1) {
+	            throw new IllegalArgumentException(
+	              "Invalid Hexadecimal Character: "+ hexChar);
+	        }
+	        return digit;
+	    }
+	    
+	    public static String byteToHex(byte num) {
+	        char[] hexDigits = new char[2];
+	        hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
+	        hexDigits[1] = Character.forDigit((num & 0xF), 16);
+	        return new String(hexDigits);
+	    }
+	    
+	    public static String printHexBinary(byte[] byteArray) {
+	        StringBuffer hexStringBuffer = new StringBuffer();
+	        for (int i = 0; i < byteArray.length; i++) {
+	            hexStringBuffer.append(byteToHex(byteArray[i]));
+	        }
+	        return hexStringBuffer.toString();
+	    }
+	    
+	    public static byte[] parseHexBinary(String hexString) {
+	        if (hexString.length() % 2 == 1) {
+	            throw new IllegalArgumentException(
+	              "Invalid hexadecimal String supplied.");
+	        }
+	        
+	        byte[] bytes = new byte[hexString.length() / 2];
+	        for (int i = 0; i < hexString.length(); i += 2) {
+	            bytes[i / 2] = hexToByte(hexString.substring(i, i + 2));
+	        }
+	        return bytes;
+	    }
+    //Pass the Public Key through this function to send it to the client
+	    private void givePK(byte[] ourPK) throws Exception
+	    {
+	    	String data = null;
+	    	Socket client = this.server.accept();
+	    	String clientAddress = client.getInetAddress().getHostAddress();
+	    	OutputStream output = client.getOutputStream();
+	    	System.out.println(ourPK);
+	    }
+	    //The server is taking in data from client. It simply gets the clients IP ADdress, and whatever message the client sends
     private void listen() throws Exception {
         String data = null;
         Socket client = this.server.accept();
@@ -39,13 +93,13 @@ public class MyServerSocket {
         kpg.initialize(256);
         KeyPair kp = kpg.generateKeyPair();
         byte[] ourPk = kp.getPublic().getEncoded();
-    	
+       
     	//When running program, be sure to put ipAddress in command line, else change args[0] to your ipAddress
-    	MyServerSocket app = new MyServerSocket(args[0]);
+    	MyServerSocket app = new MyServerSocket("192.168.0.10");
         System.out.println("\r\nRunning Server: " + 
                 "Host=" + app.getSocketAddress().getHostAddress() + 
                 " Port=" + app.getPort());
-        
+       app.givePK(ourPk);
         app.listen();
     }
 }
