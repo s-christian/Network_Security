24b53a89e2ceb82b807ec88232a3375ca38f1e60
Crtruban <55709830+Crtruban@users.noreply.github.com>
2020-11-14 15:51:35 -0500

    Update MyClientSocket.java

--- Assignment1/src/com/pgx/java/socket/MyClientSocket.java
+++ Assignment1/src/com/pgx/java/socket/MyClientSocket.java
@@ -1,4 +1,5 @@
 package com.pgx.java.socket;
+import java.io.Console;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
@@ -9,6 +10,48 @@ import java.util.Scanner;
 public class MyClientSocket {
     private Socket socket;
     private Scanner scanner;
+    public static byte hexToByte(String hexString) {
+        int firstDigit = toDigit(hexString.charAt(0));
+        int secondDigit = toDigit(hexString.charAt(1));
+        return (byte) ((firstDigit << 4) + secondDigit);
+    }
+    
+    private static int toDigit(char hexChar) {
+        int digit = Character.digit(hexChar, 16);
+        if(digit == -1) {
+            throw new IllegalArgumentException(
+              "Invalid Hexadecimal Character: "+ hexChar);
+        }
+        return digit;
+    }
+    
+    public static String byteToHex(byte num) {
+        char[] hexDigits = new char[2];
+        hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
+        hexDigits[1] = Character.forDigit((num & 0xF), 16);
+        return new String(hexDigits);
+    }
+    
+    public static String printHexBinary(byte[] byteArray) {
+        StringBuffer hexStringBuffer = new StringBuffer();
+        for (int i = 0; i < byteArray.length; i++) {
+            hexStringBuffer.append(byteToHex(byteArray[i]));
+        }
+        return hexStringBuffer.toString();
+    }
+    
+    public static byte[] parseHexBinary(String hexString) {
+        if (hexString.length() % 2 == 1) {
+            throw new IllegalArgumentException(
+              "Invalid hexadecimal String supplied.");
+        }
+        
+        byte[] bytes = new byte[hexString.length() / 2];
+        for (int i = 0; i < hexString.length(); i += 2) {
+            bytes[i / 2] = hexToByte(hexString.substring(i, i + 2));
+        }
+        return bytes;
+    }
     private MyClientSocket(InetAddress serverAddress, int serverPort) throws Exception {
         this.socket = new Socket(serverAddress, serverPort);
         this.scanner = new Scanner(System.in);
@@ -27,6 +70,9 @@ public class MyClientSocket {
         MyClientSocket client = new MyClientSocket(
                 InetAddress.getByName(args[0]), 
                 Integer.parseInt(args[1]));
+        //Generate clients Public Key
+        Console console = System.console();
+        byte[] otherPk = parseHexBinary(console.readLine("Other PK: "));
         //1.1 Generate a random string
         byte[] array = new byte[7]; // length is bounded by 7
         new Random().nextBytes(array);
