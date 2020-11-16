daf96b1ebab0b35726da1e62f5d3bc831f7c1088
Crtruban <55709830+Crtruban@users.noreply.github.com>
2020-11-16 18:52:57 -0500

    Update ecdh.java

--- Assignment1/src/com/pgx/java/socket/ecdh.java
+++ Assignment1/src/com/pgx/java/socket/ecdh.java
@@ -4,14 +4,69 @@ import java.security.spec.ECParameterSpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.security.interfaces.ECPublicKey;
 
+import javax.crypto.Cipher;
 import javax.crypto.KeyAgreement;
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.*;
+import java.util.Base64;
 import java.util.*;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.io.Console;
 
 
 public class ecdh {
+	
+	   private static PrivateKey privateKey;
+	    private PublicKey publicKey;
+	    private static final String ALGO = "AES";
+	    public ecdh() throws NoSuchAlgorithmException {
+	        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+	        keyGen.initialize(1024);
+	        KeyPair pair = keyGen.generateKeyPair();
+	        this.privateKey = pair.getPrivate();
+	        this.publicKey = pair.getPublic();
+	    }
+
+	    public void writeToFile(String path, byte[] key) throws IOException {
+	        File f = new File(path);
+	        f.getParentFile().mkdirs();
+
+	        FileOutputStream fos = new FileOutputStream(f);
+	        fos.write(key);
+	        fos.flush();
+	        fos.close();
+	    }
+
+	    public static PrivateKey getPrivateKey() {
+	        return privateKey;
+	    }
+	    public static byte[] encrypt(byte[] Data) throws Exception {
+	        Key key = getPrivateKey();
+	        Cipher c = Cipher.getInstance(ALGO);
+	        c.init(Cipher.ENCRYPT_MODE, key);
+	        byte[] encVal = c.doFinal(Data);
+	        //String encryptedValue = new BASE64Encoder().encode(encVal);
+	        return encVal;
+	    }
+
+	    public static byte[] decrypt(byte[] encryptedData) throws Exception {
+	        Key key = getPrivateKey();
+	        Cipher c = Cipher.getInstance(ALGO);
+	        c.init(Cipher.DECRYPT_MODE, key);
+
+	        byte[] decValue = c.doFinal(encryptedData);
+	        return decValue;
+	    }
+
+	    public PublicKey getPublicKey() {
+	        return publicKey;
+	    }
+	
+	
 	   public static byte hexToByte(String hexString) {
 	        int firstDigit = toDigit(hexString.charAt(0));
 	        int secondDigit = toDigit(hexString.charAt(1));
@@ -66,8 +121,9 @@ public class ecdh {
     console.printf("Public Key: %s%n", printHexBinary(ourPk));
 
     // Read other's public key:
-    byte[] otherPk = parseHexBinary(console.readLine("Other PK: "));
-
+   // byte[] otherPk = parseHexBinary(console.readLine("Other PK: "));
+    byte[] otherPk = kp.getPublic().getEncoded();
+   console.printf("Other Key: %s%n", printHexBinary(otherPk));
     KeyFactory kf = KeyFactory.getInstance("EC");
     X509EncodedKeySpec pkSpec = new X509EncodedKeySpec(otherPk);
     PublicKey otherPublicKey = kf.generatePublic(pkSpec);
@@ -92,5 +148,17 @@ public class ecdh {
 
     byte[] derivedKey = hash.digest();
     console.printf("Final key: %s%n", printHexBinary(derivedKey));
+    ByteBuffer wrapped = ByteBuffer.wrap(sharedSecret); // big-endian by default
+    ByteBuffer depparw = ByteBuffer.wrap(derivedKey); // big-endian by default
+    int num = wrapped.getShort(); 
+    int mun = depparw.getShort();
+
+    ByteBuffer dbuf = ByteBuffer.allocate(derivedKey.length+sharedSecret.length);
+    num = num + mun;
+    dbuf.putInt(num);
+    byte[] bytes = dbuf.array(); 
+    
+    console.printf("Encryper Secret: %s%n", printHexBinary(bytes));
+    
   }
 }
